9a6fe1def822d2cb11a7cbe9b7aa373ce0ccfd9a
Jenna Atkinson <>
2021-12-17 13:50:01 -0700

    Grid styling + vote

--- src/components/ImageGrid.vue
+++ src/components/ImageGrid.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="imageGrid">
     <div class="imageItem" v-for="image in imageList" :key="image.id">
-      <div class="imageViewTop">
-        <div class="voteButtons">
-          <button><i class="fas fa-caret-up"></i></button>
-          <p>{{image.voteCounter}}</p>
-          <button><i class="fas fa-caret-down"></i></button>
-        </div>
-        <img :src="require('../assets/images/' + image.path)" class="imagePic"/>
+      <div class="voteButtons">
+        <button @click="incrementVote(image)"><i class="fas fa-caret-up"></i></button>
+        <p>{{voteCounter(image)}}</p>
+        <button @click="decrementVote(image)"><i class="fas fa-caret-down"></i></button>
       </div>
-      <p class="description">{{image.description}}</p>
+      <div class="imagePic" :style="{ backgroundImage: 'url(' + require(`@/assets/images/${image.path}`) + ')' }">
+      </div>
+      <div class="descriptionView">
+        <p>{{image.description}}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -24,42 +25,54 @@ export default {
     return {
     }
   },
+  methods: {
+    voteCounter(image) {
+      return this.$root.$data.imageList[image.id].voteCounter;
+    },
+    incrementVote(image) {
+      this.$root.$data.imageList[image.id].voteCounter++;
+    },
+    decrementVote(image) {
+      this.$root.$data.imageList[image.id].voteCounter--;
+    }
+  }
   
 }
 </script>
 
 <style scoped>
-.imageViewTop {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr;
-  gap: 10px 10px;
-  grid-auto-flow: row;
-  grid-template-areas:
-    ". .";
-}
-.voteButtons {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
-  gap: 10px 10px 10px;
-  grid-auto-flow: row;
-  grid-template-areas:
-    "."
-    "."
-    ".";
-}
 .imageGrid {
+  width: 1000px;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr 1fr;
-  gap: 10px 10px;
+  gap: 50px 2px;
   grid-auto-flow: row;
   grid-template-areas:
     ". . ."
     ". . .";
 }
-.imagePic {
-  width: 100px;
+.imageItem {
+  width: 300px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1.3fr 0.7fr 1.3fr 0.7fr;
+  gap: 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "voteButtons imagePic imagePic imagePic imagePic imagePic imagePic"
+    "voteButtons imagePic imagePic imagePic imagePic imagePic imagePic"
+    "voteButtons imagePic imagePic imagePic imagePic imagePic imagePic"
+    ". descriptionView descriptionView descriptionView descriptionView descriptionView descriptionView";
 }
+
+.voteButtons { grid-area: voteButtons; }
+
+.imagePic {
+  grid-area: imagePic;
+  background-size: cover;
+}
+
+.descriptionView { grid-area: descriptionView; }
+
 </style>
