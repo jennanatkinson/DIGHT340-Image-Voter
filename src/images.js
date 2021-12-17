11a52ad7dfa814befc265e6a8150f0556b0afe93
Jenna Atkinson <>
2021-12-16 21:22:58 -0700

    Created image source file

---
+++ src/images.js
@@ -0,0 +1,26 @@
+const imageList = [
+  {
+    path: 'artanis.jpg',
+    description: 'This is Artanis'
+  },
+  {
+    path: 'arthas.jpg',
+    description: 'This is Arthas'
+  },
+  {
+    path: 'auriel.jpg',
+    description: 'This is Auriel'
+  },
+  {
+    path: 'blaze.jpg',
+    description: 'This is Blaze'
+  },
+  {
+    path: 'brightwing.jpg',
+    description: 'This is Brightwing'
+  },
+  {
+    path: 'cassia.jpg',
+    description: 'This is Cassia'
+  },
+];
